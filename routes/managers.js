07d015339c9430c3bc798673b2512f6146e514ed
Dennis Beatty <dennis@dcbeatty.com>
2015-06-18 23:31:16 -0600

    managers endpoints

---
+++ routes/managers.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+	router = express.Router(),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User'),
+	Manager = mongoose.model('Manager');
+
+/* GET list of all managers */
+router.get('/', function(req, res, next) {
+	Manager.find({ '_type' : 'Manager' }, function(err, managers){
+		if (err)
+			return next(err);
+
+		res.json(managers);
+	});
+});
+
+/* GET specified manager */
+router.get('/:manager', function(req, res) {
+	res.json(req.manager);
+});
+
+/* GET list of all properties belonging to a manager */
+router.get('/:manager/properties', function(req, res, next) {
+	req.manager.populate('properties', function(err, manager) {
+		if (err)
+			return next(err);
+		res.json(manager);
+	});
+})
+
+/* POST a new manager */
+router.post('/', function(req, res, next) {
+	if (!req.body.username || !req.body.password)
+		return res.status(400).json({ message: 'Please fill out all fields' });
+	
+	var manager = new Manager();
+	manager.username = req.body.username;
+	manager.setPassword(req.body.password);
+
+	manager.save(function(err) {
+		if (err)
+			return next(err);
+		return res.json(manager);
+	});
+});
+
+/* Get manager object when a manager param is supplied */
+router.param('manager', function(req, res, next, id) {
+	var query = Manager.findById(id);
+	query.exec(function (err, manager){
+		if (err)
+			return next(err);
+		if (!manager)
+			return next(new Error('Can\'t find manager'));
+
+		req.manager = manager;
+		return next();
+	});
+});
+
+module.exports = router;
