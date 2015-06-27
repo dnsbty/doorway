34ad7eb88228bf4b20f2b368f16dd78fd80ee440
Dennis Beatty <dennis@dcbeatty.com>
2015-06-26 22:01:42 -0600

    generate stripe connect URLs

--- models/Owners.js
+++ models/Owners.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	jwt = require('jsonwebtoken');
 
 var OwnerSchema = new mongoose.Schema({
 	name: String,
@@ -8,5 +9,14 @@ var OwnerSchema = new mongoose.Schema({
 	stripe_access: String,
 	stripe_refresh: String
 });
+
+OwnerSchema.methods.toJSON = function() {
+	var owner = this.toObject();
+	if (!owner.stripe_id || owner.stripe_id == '')
+		owner.stripe_connect_url = 'https://connect.stripe.com/oauth/authorize?response_type=code'
+			+ '&client_id=' + process.env.STRIPE_CONNECT + '&scope=read_write'
+			+ '&state=' + jwt.sign({ owner: this._id }, process.env.JWT_SECRET);
+	return owner;
+};
 
 mongoose.model('Owner', OwnerSchema);
--- public/js/controllers/OwnerController.js
+++ public/js/controllers/OwnerController.js
@@ -1,10 +1,15 @@
 app.controller('OwnerController', [
 	'$scope',
 	'$state',
+	'$window',
 	'auth',
 	'owner',
-	function($scope, $state, auth, owner) {
+	function($scope, $state, $window, auth, owner) {
 		$scope.user = auth.currentUser();
 		$scope.owner = owner;
+
+		$scope.stripeConnect = function() {
+			$window.location.href = owner.stripe_connect_url;
+		};
 	}
 ]);
