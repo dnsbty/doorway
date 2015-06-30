f7ddafbcda58cb593f0c00d6c47bcc78fe986949
Dennis Beatty <dennis@dcbeatty.com>
2015-06-30 14:42:17 -0600

    create payments

--- routes/payments.js
+++ routes/payments.js
@@ -2,6 +2,10 @@ var express = require('express'),
 	router = express.Router(),
 	mongoose = require('mongoose'),
 	Payment = mongoose.model('Payment'),
+	Tenant = mongoose.model('Tenant'),
+	Account = mongoose.model('Account'),
+	Property = mongoose.model('Property'),
+	Owner = mongoose.model('Owner'),
 	stripe = require('stripe')(process.env.STRIPE_SECRET),
 	mandrill_lib = require('mandrill-api/mandrill'),
 	mandrill = new mandrill_lib.Mandrill(process.env.MANDRILL_KEY),
@@ -24,6 +28,62 @@ router.get('/', auth, function(req, res, next) {
 /* GET specified payment */
 router.get('/:payment', auth, function(req, res, next) {
 	res.json(req.payment);
+});
+
+/* POST a new payment */
+router.post('/', auth, function(req, res, next) {
+	Tenant.findById(req.payload._id, function(err, tenant) {
+		if (err)
+			return next(err);
+
+		Property.findById(tenant.property, function(err, property) {
+			if (err)
+				return next(err);
+
+			Owner.findById(property.owner, function(err, owner) {
+				if (err)
+					return next(err);
+
+				Account.findOne({ owner: tenant, validated: true }, function(err, account) {
+					if (err)
+						return next(err);
+
+					stripe.tokens.create({
+						customer: tenant.stripe_customer,
+						bank_account: account.token
+					},
+					owner.stripe_access,
+					function(err, token) {
+						if (err)
+							return next(err);
+
+						stripe.charges.create({
+							amount: property.rent * 100,
+							currency: 'usd',
+							source: token.id,
+							description: 'Rent payment',
+							application_fee: 300
+						},
+						owner.stripe_access,
+						function(err, charge) {
+							if (err)
+								return next(err);
+
+							var payment = new Payment({
+								stripe_charge: charge.id,
+								amount: charge.amount / 100,
+								created: Date(charge.created * 1000),
+								source: account,
+								property: property
+							});
+							payment.save();
+							res.json(payment);
+						});
+					});
+				});
+			});
+		});
+	});
 });
 
 /* Get payment object when a payment param is supplied */
