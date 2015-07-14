c550d3d387081592f266c3dd3c52fb9bd97e7404
Dennis Beatty <dennis@dcbeatty.com>
2015-07-14 09:08:38 -0600

    save application submitted time

--- models/Applications.js
+++ models/Applications.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var ApplicationSchema = new mongoose.Schema({
-	property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' }
+	property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' },
+	submitted: { type: Date, default: Date.now }
 },{ "strict": false });
 
 mongoose.model('Application', ApplicationSchema);
