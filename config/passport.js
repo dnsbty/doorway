c2212e859b3bce2e72c8179948899407d2c6dc69
Dennis Beatty <dennis@dcbeatty.com>
2015-11-19 00:12:02 -0700

    use emails for demo logins

--- config/passport.js
+++ config/passport.js
@@ -5,10 +5,6 @@ var User = mongoose.model('User');
 
 passport.use(new LocalStrategy(
 	function(username, password, done) {
-		if (username == "landlord")
-			username = "landlord_demo@doorwayapp.com";
-		else if (username == "tenant")
-			username = "tenant_demo@doorwayapp.com";
 		User.findOne({ email: username }, function (err, user) {
 			if (err) { return done(err); }
 			if (!user) {
